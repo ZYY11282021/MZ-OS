7270e4a3705594c6da4689edfbebc13ae5ac82ba
ZYY <95468604+ZYY11282021@users.noreply.github.com>
2022-05-20 19:36:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import requests
 
 print("欢迎来到MZ-OS!")
 print("如果你需要一些帮助，请输入“帮助”")
@@ -17,6 +18,19 @@ def oput():
         print("退出: 退出MZ-OS")
         print("清屏: 清空屏幕")
         print("帮助: 显示此界面")
+    elif iputContent[:2] == "下载":
+        spLst = iputContent.split()
+        req = requests.get(spLst[1])
+        filename = spLst[1].split('/')[-1]
+        if req.status_code != 200:
+            print("下载异常，请检查您的网络连接或输入的网址")
+        try:
+            with open(filename, "wb") as f:
+                f.write(req.content)
+                print("下载成功")
+        except Exception as e:'
+            print("下载失败 错误信息:", e)
+        
     else:
         print("找不到命令“%s”" % iputContent)
         
